d1d26de019887ffc47105a81da2fa0128bb5571a
Arnaud Dartois <nonobis@gmail.com>
2018-02-27 14:41:28 +0100

    Update Package Version

--- SimpleExtension/Properties/AssemblyInfo.cs
+++ SimpleExtension/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.24")]
-[assembly: AssemblyFileVersion("0.0.24")]
+[assembly: AssemblyVersion("0.0.25")]
+[assembly: AssemblyFileVersion("0.0.25")]
