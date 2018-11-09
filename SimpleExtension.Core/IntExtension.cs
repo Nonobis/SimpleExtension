390c3ae84191be17fb69612ee6ffac58aed30a1a
Arnaud Dartois <nonobis@gmail.com>
2018-11-09 13:54:47 +0100

    Migrate to NetStandard 2.0 Add new Extension Method for AspNetCore Removed Alpha Tag Upgraded Version to 0.0.5

--- SimpleExtension.Core/IntExtension.cs
+++ SimpleExtension.Core/IntExtension.cs
@@ -5,6 +5,31 @@ namespace SimpleExtension.Core
     public static class IntExtension
     {
         /// <summary>
+        /// Betweens the specified p maximum.
+        /// </summary>
+        /// <param name="pMin">The p minimum.</param>
+        /// <param name="pMax">The p maximum.</param>
+        /// <returns></returns>
+        public static bool Between(this int pMin, int pMax)
+        {
+            return pMax > pMin;
+        }
+
+        /// <summary>
+        /// Determines whether the specified minimum is within.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="minimum">The minimum.</param>
+        /// <param name="maximum">The maximum.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified minimum is within; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsWithin(this int value, int minimum, int maximum)
+        {
+            return value >= minimum && value <= maximum;
+        }
+
+        /// <summary>
         /// Return number of digits in a integer
         /// </summary>
         /// <param name="pNumber"></param>
--- SimpleExtension.Core/StringExtension.cs
+++ SimpleExtension.Core/StringExtension.cs
@@ -10,84 +10,19 @@ namespace SimpleExtension.Core
     {
 
         /// <summary>
-        ///     Strings to bytes.
-        /// </summary>
-        /// <param name="str">The string.</param>
-        /// <returns>System.Byte[].</returns>
-        public static byte[] StringToBytes(this string str)
-        {
-            var bytes = new byte[str.Length * sizeof(char)];
-            Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-            return bytes;
-        }
-
-        /// <summary>
-        ///     Gets the double.
-        /// </summary>
-        public static double GetDouble(this string value, double defaultValue)
-        {
-            //Try parsing in the current culture
-            if (!double.TryParse(value, NumberStyles.Any, CultureInfo.CurrentCulture, out var result) &&
-                //Then try in US english
-                !double.TryParse(value, NumberStyles.Any, new CultureInfo("en-US"), out result) &&
-                //Then in neutral language
-                !double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
-                result = defaultValue;
-            return result;
-        }
-
-
-        /// <summary>
-        ///     takes a substring between two anchor strings (or the end of the string if that anchor is null)
-        /// </summary>
-        public static string Substring(this string pSource, string from = null, string until = null,
-            StringComparison comparison = StringComparison.CurrentCulture)
-        {
-            var fromLength = (from ?? string.Empty).Length;
-            var startIndex = !string.IsNullOrEmpty(from)
-                ? pSource.IndexOf(from, comparison) + fromLength
-                : 0;
-
-            if (startIndex < fromLength)
-                throw new ArgumentException("from: Failed to find an instance of the first anchor");
-
-            var endIndex = !string.IsNullOrEmpty(until)
-                ? pSource.IndexOf(until, startIndex, comparison)
-                : pSource.Length;
-
-            if (endIndex < 0) throw new ArgumentException("until: Failed to find an instance of the last anchor");
-
-            var subString = pSource.Substring(startIndex, endIndex - startIndex);
-            return subString;
-        }
-
-        /// <summary>
-        /// Convert string to Stram
-        /// </summary>
-        public static Stream ToStream(this string pValue)
-        {
-            // convert string to stream
-            if (pValue == null) return null;
-            var byteArray = Encoding.UTF8.GetBytes(pValue);
-
-            var stream = new MemoryStream(byteArray);
-            return stream;
-        }
-
-        /// <summary>
         ///   Convert Hexadecimal to string
         /// </summary>
         public static byte[] BinHexToString(this string hex)
         {
             var offset = hex.StartsWith("0x", StringComparison.Ordinal) ? 2 : 0;
-            if (hex.Length%2 != 0)
+            if (hex.Length % 2 != 0)
                 throw new ArgumentException($"Invalid Hex String : {hex.Length}");
 
-            var ret = new byte[(hex.Length - offset)/2];
+            var ret = new byte[(hex.Length - offset) / 2];
 
             for (var i = 0; i < ret.Length; i++)
             {
-                ret[i] = (byte) ((ParseHexChar(hex[offset]) << 4) |
+                ret[i] = (byte)((ParseHexChar(hex[offset]) << 4) |
                                  ParseHexChar(hex[offset + 1]));
                 offset += 2;
             }
@@ -152,6 +87,42 @@ namespace SimpleExtension.Core
         }
 
         /// <summary>
+        ///     Gets the double.
+        /// </summary>
+        public static double GetDouble(this string value, double defaultValue)
+        {
+            //Try parsing in the current culture
+            if (!double.TryParse(value, NumberStyles.Any, CultureInfo.CurrentCulture, out var result) &&
+                //Then try in US english
+                !double.TryParse(value, NumberStyles.Any, new CultureInfo("en-US"), out result) &&
+                //Then in neutral language
+                !double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+                result = defaultValue;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the until or empty.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="stopAt">The stop at.</param>
+        /// <returns></returns>
+        public static string GetUntilOrEmpty(this string text, string stopAt = "-")
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+
+                if (charLocation > 0)
+                {
+                    return text.Substring(0, charLocation);
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
         ///     HTML-encodes a string and returns the encoded string.
         /// </summary>
         public static string HtmlEncode(this string text)
@@ -182,7 +153,7 @@ namespace SimpleExtension.Core
                         {
                             // decimal numeric entity
                             sb.Append("&#");
-                            sb.Append(((int) text[i]).ToString(CultureInfo.InvariantCulture));
+                            sb.Append(((int)text[i]).ToString(CultureInfo.InvariantCulture));
                             sb.Append(";");
                         }
                         else
@@ -248,61 +219,6 @@ namespace SimpleExtension.Core
         public static string ProperCase(this string pInput, bool pAddPointAfterSingleLetter)
         {
             return InternalProperCase(pInput, pAddPointAfterSingleLetter);
-        }
-
-        /// <summary>
-        ///     Return a string in proper Case format
-        /// </summary>
-        /// <param name="pInput">Text to format as string.</param>
-        /// <param name="pAddPointAfterSingleLetter">Add a period if desired.</param>
-        private static string InternalProperCase(string pInput, bool pAddPointAfterSingleLetter)
-        {
-            //This code is modified from the http://www.windowsdevcenter.com/pub/a/oreilly/windows/news/csharp_0101.html link and related posts.
-
-            var pattern = @"\w+|\W+";
-            var result = "";
-            var capitalizeNext = true;
-
-            foreach (Match m in Regex.Matches(pInput, pattern))
-            {
-                // get the matched string
-                var x = m.ToString().ToLower();
-
-                // if the first char is lower case
-                if (char.IsLower(x[0]) && capitalizeNext)
-                {
-                    // capitalize it
-                    x = char.ToUpper(x[0]) + x.Substring(1, x.Length - 1);
-                }
-
-                // Check if the word starts with Mc
-                //if (x[0] == 'M' && x[1] == 'c' && !String.IsNullOrEmpty(x[2].ToString()))
-                if (x[0] == 'M' && x.Length > 1 && x[1] == 'c' && !string.IsNullOrEmpty(x[2].ToString()))
-                {
-                    // Capitalize the letter after Mc
-                    x = "Mc" + char.ToUpper(x[2]) + x.Substring(3, x.Length - 3);
-                }
-
-                if (capitalizeNext == false)
-                    capitalizeNext = true;
-
-                // if the apostrophe is at the end i.e. Andrew's
-                // then do not capitalize the next letter
-                if (x[0].ToString() == "'" && m.NextMatch().ToString().Length == 1)
-                {
-                    capitalizeNext = false;
-                }
-
-                // collect all text
-                result += x;
-            }
-
-            if ((pAddPointAfterSingleLetter) && (result.Length == 1))
-            {
-                result = result + ".";
-            }
-
-            return result;
         }
 
         /// <summary>
@@ -398,12 +314,23 @@ namespace SimpleExtension.Core
         }
 
         /// <summary>
+        ///     Strings to bytes.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>System.Byte[].</returns>
+        public static byte[] StringToBytes(this string str)
+        {
+            var bytes = new byte[str.Length * sizeof(char)];
+            Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+        /// <summary>
         ///     Creates a Stream from a string. Internally creates
         ///     a memory stream and returns that.
         /// </summary>
         public static Stream StringToStream(this string text, Encoding encoding)
         {
-            var ms = new MemoryStream(text.Length*2);
+            var ms = new MemoryStream(text.Length * 2);
             var data = encoding.GetBytes(text);
             ms.Write(data, 0, data.Length);
             ms.Position = 0;
@@ -444,6 +371,30 @@ namespace SimpleExtension.Core
                     sb.Append(chr);
 
             return sb.ToString();
+        }
+
+        /// <summary>
+        ///     takes a substring between two anchor strings (or the end of the string if that anchor is null)
+        /// </summary>
+        public static string Substring(this string pSource, string from = null, string until = null,
+            StringComparison comparison = StringComparison.CurrentCulture)
+        {
+            var fromLength = (from ?? string.Empty).Length;
+            var startIndex = !string.IsNullOrEmpty(from)
+                ? pSource.IndexOf(from, comparison) + fromLength
+                : 0;
+
+            if (startIndex < fromLength)
+                throw new ArgumentException("from: Failed to find an instance of the first anchor");
+
+            var endIndex = !string.IsNullOrEmpty(until)
+                ? pSource.IndexOf(until, startIndex, comparison)
+                : pSource.Length;
+
+            if (endIndex < 0) throw new ArgumentException("until: Failed to find an instance of the last anchor");
+
+            var subString = pSource.Substring(startIndex, endIndex - startIndex);
+            return subString;
         }
 
         /// <summary>
@@ -503,6 +454,18 @@ namespace SimpleExtension.Core
         }
 
         /// <summary>
+        /// Convert string to Stram
+        /// </summary>
+        public static Stream ToStream(this string pValue)
+        {
+            // convert string to stream
+            if (pValue == null) return null;
+            var byteArray = Encoding.UTF8.GetBytes(pValue);
+
+            var stream = new MemoryStream(byteArray);
+            return stream;
+        }
+        /// <summary>
         ///     Trims a sub string from a string
         /// </summary>
         public static string TrimStart(this string text, string textToTrim, bool caseInsensitive)
@@ -533,6 +496,60 @@ namespace SimpleExtension.Core
             return value;
         }
 
+        /// <summary>
+        ///     Return a string in proper Case format
+        /// </summary>
+        /// <param name="pInput">Text to format as string.</param>
+        /// <param name="pAddPointAfterSingleLetter">Add a period if desired.</param>
+        private static string InternalProperCase(string pInput, bool pAddPointAfterSingleLetter)
+        {
+            //This code is modified from the http://www.windowsdevcenter.com/pub/a/oreilly/windows/news/csharp_0101.html link and related posts.
+
+            var pattern = @"\w+|\W+";
+            var result = "";
+            var capitalizeNext = true;
+
+            foreach (Match m in Regex.Matches(pInput, pattern))
+            {
+                // get the matched string
+                var x = m.ToString().ToLower();
+
+                // if the first char is lower case
+                if (char.IsLower(x[0]) && capitalizeNext)
+                {
+                    // capitalize it
+                    x = char.ToUpper(x[0]) + x.Substring(1, x.Length - 1);
+                }
+
+                // Check if the word starts with Mc
+                //if (x[0] == 'M' && x[1] == 'c' && !String.IsNullOrEmpty(x[2].ToString()))
+                if (x[0] == 'M' && x.Length > 1 && x[1] == 'c' && !string.IsNullOrEmpty(x[2].ToString()))
+                {
+                    // Capitalize the letter after Mc
+                    x = "Mc" + char.ToUpper(x[2]) + x.Substring(3, x.Length - 3);
+                }
+
+                if (capitalizeNext == false)
+                    capitalizeNext = true;
+
+                // if the apostrophe is at the end i.e. Andrew's
+                // then do not capitalize the next letter
+                if (x[0].ToString() == "'" && m.NextMatch().ToString().Length == 1)
+                {
+                    capitalizeNext = false;
+                }
+
+                // collect all text
+                result += x;
+            }
+
+            if ((pAddPointAfterSingleLetter) && (result.Length == 1))
+            {
+                result = result + ".";
+            }
+
+            return result;
+        }
         /// <summary>
         ///     Parses the hexadecimal character.
         /// </summary>
